d03ce9d7fa4594cf609ca2b5c3f9ac224d26b553
ridelyEX <arturovalruiz@gmail.com>
2024-11-25 01:32:35 -0600

    botones modificados

--- src/app/components/TitleCard.js
+++ src/app/components/TitleCard.js
@@ -8,9 +8,28 @@ const TitleCard = () => {
       <nav>
       <div className="title-text-container">
         <h1 className="title-text">Movie Guesser</h1>
-        <Link href="MainPage">Play</Link>
-        <Link href="/LoginPage">Iniciar Sesion</Link>
-        <Link href="/SignUpPage">Registrarse</Link>
+        <div className="flex flex-col space-y-4 w-64">
+            <Link 
+              href="/MainPage"
+              className="px-6 py-2 text-lg font-semibold text-black bg-red-600 rounded-lg hover:bg-red-700 transition-colors duration-200 text-center"
+            >
+              Play
+            </Link>
+            
+            <Link 
+              href="/LoginPage"
+              className="px-6 py-2 text-lg font-semibold text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors duration-200 text-center"
+            >
+              Iniciar Sesión
+            </Link>
+            
+            <Link 
+              href="/SignUpPage"
+              className="px-6 py-2 text-lg font-semibold text-white bg-purple-600 rounded-lg hover:bg-purple-700 transition-colors duration-200 text-center"
+            >
+              Registrarse
+            </Link>
+          </div>
       </div>
       </nav>
     </div>
